9ff72589742b588200fb88e15c8662fd3f636d7b
Kyle <dotnetKyle@gmail.com>
2021-02-05 17:56:13 -0500

    Works, needs further testing and needs to be sped up some. The Average time test has it at about 75 ticks, would be good to get it somewhat faster.

---
+++ src/Stencil/Rule.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Stencil
+{
+    public class Rule
+    {
+        private Rule() { }
+
+        public string RuleName { get; private set; }
+        public string RuleNameLowerCase { get; private set; }
+        public string RuleNameLowerCaseWithBrackets { get; private set; }
+        public string PropertyName { get; private set; }
+        public string Stencil { get; private set; }
+        public Func<object, string> Setup { get; private set; }
+
+        public static Rule New(string ruleName, string stencil = "", string propertyName = "", Func<object, string> setup = null)
+        {
+            if (string.IsNullOrWhiteSpace(ruleName))
+                throw new ArgumentException("The name of the rule is required.", nameof(ruleName));
+
+            return new Rule
+            {
+                RuleName = ruleName,
+                RuleNameLowerCase = ruleName.ToLower(),
+                RuleNameLowerCaseWithBrackets = '{' + ruleName.ToLower() + '}',
+
+                PropertyName = string.IsNullOrWhiteSpace(propertyName)
+                    ? ruleName.ToLower()
+                    : propertyName.ToLower(),
+
+                Setup = setup,
+
+                Stencil = string.IsNullOrWhiteSpace(stencil)
+                    ? "{}"
+                    : stencil
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"Rule:{{{RuleName}}}";
+        }
+    }
+
+}
---
+++ src/Stencil/TemplateEngine.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Stencil
+{
+    public class TemplateEngine
+    {
+        public TemplateEngine()
+        {
+            _rules = new List<Rule>();
+        }
+
+        List<Rule> _rules;
+
+        public string GetResult(string template, object o)
+        {
+            var sb = new StringBuilder();
+            var props = o.GetType().GetProperties();
+
+            var x = new List<char>();
+
+            using(var reader = new StringReader(template))
+            {
+                while(true)
+                {
+                    //var charNum = reader.Read();
+                    var c = reader.Read();
+
+                    if (c == -1)
+                        break;
+
+                    if (c == '{')
+                    {
+                        // in property
+                        var ruleName = "";
+                        while(c != '}') // read until done
+                        {
+                            c = (char)reader.Read();
+                            if(c == '}')
+                            {
+                                c = (char)reader.Read();
+                                break;
+                            }
+                            ruleName += Char.ToLower((char)c);
+                        }
+
+                        var rule = _rules.FirstOrDefault(r => r.RuleNameLowerCase == ruleName);
+
+                        if(rule != null)
+                        {
+                            var prop = props.FirstOrDefault(p => p.Name.ToLower() == rule.PropertyName);
+
+                            var val = (rule.Setup != null)
+                                ? rule.Setup(prop.GetValue(o))
+                                : prop.GetValue(o).ToString();
+
+                            for (int i = 0; i < rule.Stencil.Length; i++)
+                            {
+                                if (rule.Stencil[i] == '{')
+                                {
+                                    sb.Append(val);
+                                    i++; // skip end bracket
+                                }
+                                else
+                                {
+                                    sb.Append(rule.Stencil[i]);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // add characters to the template
+                        sb.Append(c);
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public TemplateEngine AddRule(Rule rule)
+        {
+            _rules.Add(rule);
+
+            return this;
+        }
+
+        public TemplateEngine AddRule(string name, string stencil = "", string propertyName = "", Func<object, string> setup = null)
+            => AddRule(Rule.New(name, 
+                stencil:stencil, 
+                propertyName:propertyName, 
+                setup:setup));
+    }
+}
